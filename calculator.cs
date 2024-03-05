33a0b12dc5618f0ae2c7c0004e4741815c15497d
Windyarto <windyartohaqi@gmail.com>
2024-03-05 10:19:48 +0700

    memperbarui code

---
+++ calculator.cs
@@ -0,0 +1,13 @@
+class Calculator
+{
+	static void Main(sting[]args)
+	{
+		int a =10;
+		int b =6;
+		
+		Console.WriteLine(Hasil Penambahan: {0} + {1} ={2}",a,b,Penambahan(a,b));
+		Console.WriteLine(Hasil Penambahan: {0} - {1} ={2}",a,b,Pengurangan(a,b));
+		
+		Console.WriteLine("\nTekan
+	}
+}
